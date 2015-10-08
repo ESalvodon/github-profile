29a4d8d6ed6272097fe790e597682e29da6dee6d
Victor Alvarez <victor.alvarez.iii@gmail.com>
2015-10-07 21:43:03 -0400

    vic's commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,14 @@
 import $ from 'jquery';
 import Router from './router';
 
-
-
 $(() => {
-  new Router();
-});
+  let router = new Router();
+
+
+$('#searchForm').submit(function(event) {
+      var userName = $('#userSearch').val();
+      router.navigate("#" + userName, true);
+      document.getElementById('searchForm').reset();
+      return false;  //another method of doing event.preventDefault();, keeps page from refreshing
+    });
+  });
